dce17d490cc6f19fd8d6e25ada65860c9641cc23
Andrew Johnston <apjohnsto@gmail.com>
2020-11-30 19:21:44 +0000

    add wasm module. remove unnecessary cd from pg makefile

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-## run this to compile all the modules and install them in /usr/local/lib/just
-make -C blake3/ clean blake3.so install
-make -C ffi/ clean ffi.so install
-make -C html/ clean html.so install
-make -C openssl/ clean openssl.so install
-## pg currently broken - some Makefile weirdness
-#make -C pg/ clean pg.so install
-make -C picohttp/ clean http.so install
-make -C rocksdb/ clean rocksdb.so install
-make -C tinycc/ clean tcc.so install
-make -C zlib/ clean zlib.so install
+MODULES=${2:-"blake3 encode epoll ffi fs html http inspector net netlink openssl sha1 signal sys tcc thread udp vm zlib"}
+for m in $MODULES
+do
+  make -C $m/ clean library
+  sudo make -C $m/ install
+done
---
+++ wasm/wasm.cc
@@ -0,0 +1,19 @@
+#include "wasm.h"
+
+uint32_t fibonacci(uint32_t n) {
+  if (n <= 1) return 1;
+  return fibonacci(n-1) + fibonacci(n-2);
+}
+
+void just::wasm::Fibonacci(const FunctionCallbackInfo<Value> &args) {
+  Isolate *isolate = args.GetIsolate();
+  HandleScope handleScope(isolate);
+  Local<Context> context = isolate->GetCurrentContext();  
+  args.GetReturnValue().Set(Integer::New(isolate, fibonacci(args[0]->Int32Value(context).ToChecked())));
+}
+
+void just::wasm::Init(Isolate* isolate, Local<ObjectTemplate> target) {
+  Local<ObjectTemplate> module = ObjectTemplate::New(isolate);
+  SET_METHOD(isolate, module, "calculate", Fibonacci);
+  SET_MODULE(isolate, target, "wasm", module);
+}
