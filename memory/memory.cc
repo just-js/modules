88c6abd93972a8c5c86a07a709dcecbca22dfcf6
Andrew Johnston <apjohnsto@gmail.com>
2021-01-23 01:55:32 +0000

    add experimental memory module

---
+++ memory/memory.cc
@@ -0,0 +1,150 @@
+#include "memory.h"
+
+static void ResetHandleAndSetFlag(const v8::WeakCallbackInfo<just::memory::rawBuffer>& data) {
+  data.GetParameter()->buffer.Reset();
+  just::memory::buffers.erase(data.GetParameter()->handle);
+  fprintf(stderr, "free %i\n", data.GetParameter()->handle);
+  for ( const auto &p : just::memory::buffers ) {
+    fprintf(stderr, "%i\n", p.first);
+  } 
+  delete data.GetParameter();
+}
+
+void just::memory::ReadMemory(const FunctionCallbackInfo<Value> &args) {
+  Isolate *isolate = args.GetIsolate();
+  Local<BigInt> start64 = Local<BigInt>::Cast(args[0]);
+  Local<BigInt> end64 = Local<BigInt>::Cast(args[1]);
+  const uint64_t size = end64->Uint64Value() - start64->Uint64Value();
+  void* start = reinterpret_cast<void*>(start64->Uint64Value());
+  Local<ArrayBuffer> ab = ArrayBuffer::New(isolate, start, size, v8::ArrayBufferCreationMode::kExternalized);
+  args.GetReturnValue().Set(ab);
+}
+
+void just::memory::GetMeta(const FunctionCallbackInfo<Value> &args) {
+  Isolate *isolate = args.GetIsolate();
+  Local<Context> context = isolate->GetCurrentContext();
+  Local<Object> meta = args[1].As<Object>();
+  bool isExternal;
+  bool isDetachable;
+  if (args[0]->IsArrayBuffer()) {
+    Local<ArrayBuffer> buf = args[0].As<ArrayBuffer>();
+    isExternal = buf->IsExternal();
+    isDetachable = buf->IsExternal();
+  } else if (args[0]->IsSharedArrayBuffer()) {
+    Local<SharedArrayBuffer> buf = args[0].As<SharedArrayBuffer>();
+    isExternal = buf->IsExternal();
+    isDetachable = buf->IsExternal();
+  }
+  meta->Set(context, String::NewFromUtf8Literal(isolate, "isExternal", NewStringType::kNormal), v8::Boolean::New(isolate, isExternal)).Check();
+  meta->Set(context, String::NewFromUtf8Literal(isolate, "isDetachable", NewStringType::kNormal), v8::Boolean::New(isolate, isDetachable)).Check();
+  args.GetReturnValue().Set(meta);
+}
+
+void just::memory::RawBuffer(const FunctionCallbackInfo<Value> &args) {
+  Isolate *isolate = args.GetIsolate();
+  Local<ArrayBuffer> ab = args[0].As<ArrayBuffer>();
+  std::shared_ptr<BackingStore> backing = ab->GetBackingStore();
+  just::memory::rawBuffer* buf = new just::memory::rawBuffer();
+  buf->data = backing->Data();
+  buf->len = backing->ByteLength();
+  buf->buffer.Reset(isolate, ab);
+  buf->handle = just::memory::bcount;
+  buf->buffer.SetWeak(buf, &ResetHandleAndSetFlag, v8::WeakCallbackType::kFinalizer);
+  just::memory::buffers[just::memory::bcount] = buf;
+  args.GetReturnValue().Set(Integer::New(isolate, just::memory::bcount++));
+}
+
+void just::memory::Alloc(const FunctionCallbackInfo<Value> &args) {
+  Isolate *isolate = args.GetIsolate();
+  Local<Context> context = isolate->GetCurrentContext();
+  int size = Local<Integer>::Cast(args[0])->Value();
+  Local<ArrayBuffer> ab = ArrayBuffer::New(isolate, size);
+  just::memory::rawBuffer* buf = new just::memory::rawBuffer();
+  buf->data = ab->GetBackingStore()->Data();
+  buf->len = size;
+  buf->buffer.Reset(isolate, ab);
+  buf->handle = just::memory::bcount;
+  buf->buffer.SetWeak(buf, &ResetHandleAndSetFlag, v8::WeakCallbackType::kFinalizer);
+  just::memory::buffers[just::memory::bcount] = buf;
+  ab->Set(context, String::NewFromUtf8Literal(isolate, "raw", NewStringType::kNormal), Integer::New(isolate, just::memory::bcount++)).Check();
+  args.GetReturnValue().Set(ab);
+}
+
+void just::memory::WriteString(const FunctionCallbackInfo<Value> &args) {
+  Isolate *isolate = args.GetIsolate();
+  just::memory::rawBuffer* b = just::memory::buffers[Local<Integer>::Cast(args[0])->Value()];
+  Local<String> str = args[1].As<String>();
+  int len = str->Utf8Length(isolate);
+  int nchars = 0;
+  int off = 0;
+  if (args.Length() > 2) {
+    off = Local<Integer>::Cast(args[2])->Value();
+  }
+  char* dest = (char*)b->data + off;
+  args.GetReturnValue().Set(Integer::New(isolate, str->WriteUtf8(isolate, dest, len, &nchars, v8::String::HINT_MANY_WRITES_EXPECTED | v8::String::NO_NULL_TERMINATION)));
+}
+
+void just::memory::ReadString(const FunctionCallbackInfo<Value> &args) {
+  just::memory::rawBuffer* b = just::memory::buffers[Local<Integer>::Cast(args[0])->Value()];
+  int len = b->len;
+  int argc = args.Length();
+  if (argc > 1) {
+    len = Local<Integer>::Cast(args[1])->Value();
+  }
+  int off = 0;
+  if (argc > 2) {
+    off = Local<Integer>::Cast(args[2])->Value();
+  }
+  const char* src = (const char*)b->data + off;
+  args.GetReturnValue().Set(String::NewFromUtf8(args.GetIsolate(), src, 
+    NewStringType::kNormal, len).ToLocalChecked());
+}
+
+void just::memory::Copy(const FunctionCallbackInfo<Value> &args) {
+  just::memory::rawBuffer* dest = just::memory::buffers[Local<Integer>::Cast(args[0])->Value()];
+  just::memory::rawBuffer* src = just::memory::buffers[Local<Integer>::Cast(args[1])->Value()];
+  int argc = args.Length();
+  int doff = 0;
+  if (argc > 2) {
+    doff = Local<Integer>::Cast(args[2])->Value();
+  }
+  int len = src->len;
+  if (argc > 3) {
+    len = Local<Integer>::Cast(args[3])->Value();
+  }
+  int soff = 0;
+  if (argc > 4) {
+    soff = Local<Integer>::Cast(args[4])->Value();
+  }
+  char* d = (char*)dest->data + doff;
+  const char* s = (const char*)src->data + soff;
+  memcpy(d, s, len);
+  args.GetReturnValue().Set(Integer::New(args.GetIsolate(), len));
+}
+
+void just::memory::GetAddress(const FunctionCallbackInfo<Value> &args) {
+  just::memory::rawBuffer* b = just::memory::buffers[Local<Integer>::Cast(args[0])->Value()];
+  args.GetReturnValue().Set(BigInt::New(args.GetIsolate(), (uint64_t)b->data));
+}
+
+void just::memory::Init(Isolate* isolate, Local<ObjectTemplate> target) {
+  Local<ObjectTemplate> module = ObjectTemplate::New(isolate);
+
+  SET_METHOD(isolate, module, "readString", ReadString);
+  SET_METHOD(isolate, module, "writeString", WriteString);
+  SET_METHOD(isolate, module, "getAddress", GetAddress);
+  SET_METHOD(isolate, module, "rawBuffer", RawBuffer);
+  SET_METHOD(isolate, module, "readMemory", ReadMemory);
+
+  SET_METHOD(isolate, module, "getMeta", GetMeta);
+
+  SET_METHOD(isolate, module, "copy", Copy);
+  SET_METHOD(isolate, module, "alloc", Alloc);
+  //SET_METHOD(isolate, module, "free", ReadMemory);
+  //SET_METHOD(isolate, module, "shmOpen", ReadMemory);
+  //SET_METHOD(isolate, module, "shmUnlink", ReadMemory);
+  //SET_METHOD(isolate, module, "mmap", ReadMemory);
+  //SET_METHOD(isolate, module, "munmap", ReadMemory);
+
+  SET_MODULE(isolate, target, "memory", module);
+}
---
+++ memory/memory.h
@@ -0,0 +1,50 @@
+#ifndef JUST_SIGNAL_H
+#define JUST_SIGNAL_H
+
+#include <just.h>
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+#include <inttypes.h>
+#include <stdarg.h>
+
+namespace just {
+
+namespace memory {
+
+struct rawBuffer {
+	v8::Persistent<ArrayBuffer, v8::NonCopyablePersistentTraits<ArrayBuffer>> buffer;
+  void* data;
+  size_t len;
+  int handle;
+};
+
+std::map<int, rawBuffer*> buffers;
+static int bcount = 0;
+
+void ReadString(const FunctionCallbackInfo<Value> &args);
+void WriteString(const FunctionCallbackInfo<Value> &args);
+void GetAddress(const FunctionCallbackInfo<Value> &args);
+void ReadMemory(const FunctionCallbackInfo<Value> &args);
+void RawBuffer(const FunctionCallbackInfo<Value> &args);
+void Alloc(const FunctionCallbackInfo<Value> &args);
+void Copy(const FunctionCallbackInfo<Value> &args);
+void GetMeta(const FunctionCallbackInfo<Value> &args);
+
+void Init(Isolate* isolate, Local<ObjectTemplate> target);
+}
+
+}
+
+extern "C" {
+	void* _register_memory() {
+		return (void*)just::memory::Init;
+	}
+}
+
+#endif
