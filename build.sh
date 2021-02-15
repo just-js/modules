cd68edfb07b02586e3148d27deed694a203e4d3b
Andrew Johnston <apjohnsto@gmail.com>
2021-02-15 00:00:34 +0000

    c++17

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MODULES=${2:-"blake3 encode epoll ffi fs html http inspector net netlink openssl sha1 signal sys tcc thread udp vm zlib"}
+MODULES=${2:-"blake3 encode epoll ffi fs html http inspector memory net netlink openssl pg seccomp sha1 signal sys tcc thread udp vm zlib"}
 for m in $MODULES
 do
   make -C $m/ clean library
